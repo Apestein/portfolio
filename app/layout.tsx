11fd5080d2663fe9bc57de1f65500044f3071346
annony <ltn2057@protonmail.com>
2023-01-08 10:05:03 -0500

    add assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link"
 import React, { useState } from "react"
 import { DarkModeSwitch } from "react-toggle-dark-mode"
+import { DarkModeToggle } from "react-dark-mode-toggle-2"
 
 export default function RootLayout({
   children,
@@ -13,10 +14,15 @@ export default function RootLayout({
     <html>
       <head />
       <body className="grid min-h-screen grid-rows-[min-content_1fr] bg-black text-white">
-        <nav className="flex items-center justify-end gap-20 bg-indigo-900 p-3 text-xl">
+        <nav className="fixed flex w-full items-center justify-end gap-20 bg-indigo-900 p-2 text-xl">
+          {/* <DarkModeToggle
+            onChange={() => setDarkMode((prev) => !prev)}
+            isDarkMode={darkMode}
+          /> */}
           <DarkModeSwitch
             checked={darkMode}
             onChange={() => setDarkMode((prev) => !prev)}
+            className="h-10 w-10"
           />
           <Link href="/">Home</Link>
           <Link href="/blog">Blog</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import "./globals.css"
+import Image from "next/image"
+import discordDemo from "../public/discord-clone-demo.webp"
+import waldoDemo from "../public/waldo-demo.webp"
+import shoppingDemo from "../public/shopping-cart-demo.webp"
+import battleshipDemo from "../public/battleship-demo.webp"
 
 export default function Home() {
   function handleIntroAnimation(e: any) {
@@ -6,17 +11,27 @@ export default function Home() {
       document.getElementById("intro-lottie")!.style.display = "none"
   }
   return (
-    <main className="flex flex-col justify-center text-center">
-      <h1 className="text-9xl">Luc Ledo</h1>
-      <h2>Fullstack Web Developer</h2>
-      <figure>
-        <q>
-          "<span className="line-through">Chess</span> Code speaks for itself"
-        </q>
-        <figcaption>
-          &mdash;<cite>Hans Niemann</cite>
-        </figcaption>
-      </figure>
+    <main className="flex flex-col px-3">
+      <article className="grid min-h-screen place-content-center text-center">
+        <h1 className="text-9xl">Luc Ledo</h1>
+        <h2 className="mb-2 text-2xl italic">Fullstack Web Developer</h2>
+        <figure>
+          <q>
+            "<span className="line-through">Chess</span> Code speaks for itself"
+          </q>
+          <figcaption>
+            &mdash;<cite>Hans Niemann</cite>
+          </figcaption>
+        </figure>
+      </article>
+      <Image className="w-[1000px]" src={discordDemo} alt="discord-demo" />
+      <Image className="w-[1000px]" src={waldoDemo} alt="waldo-demo" />
+      <Image className="w-[1000px]" src={shoppingDemo} alt="shopping-demo" />
+      <Image
+        className="w-[1000px]"
+        src={battleshipDemo}
+        alt="battleship-demo"
+      />
     </main>
   )
 }
