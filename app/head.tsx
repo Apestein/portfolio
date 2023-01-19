aaa254dc872ab5873d9a3289d7c1f005dc702ff7
annony <ltn2057@protonmail.com>
2023-01-19 10:45:10 -0500

    add meta description

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,10 @@ export default function Head() {
     <>
       <title>Luc Ledo</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Luc Ledo Web Developer Portfolio"
+      ></meta>
       <link rel="icon" href="/favicon.ico" />
     </>
   )
